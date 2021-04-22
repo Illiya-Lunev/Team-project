0d76c42257da1c09800ef579ce16a18a275cef42
Illiya-Lunev <78380705+Illiya-Lunev@users.noreply.github.com>
2021-04-22 09:24:13 +0000

    Deploying to gh-pages from @ Illiya-Lunev/Team-project@6ef1c3e7f3c8f4c9dc219b7b146b0c1dcc25add1 🚀

--- src.10e01b45.js
+++ src.10e01b45.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/Team-project/src.8730253c.js.map
+//# sourceMappingURL=/Team-project/src.10e01b45.js.map
