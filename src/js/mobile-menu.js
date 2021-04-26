09904b71ca0a088c9916f4028d3af3766303a9d3
Vlad <nesslaen123@gmail.com>
2021-04-26 19:11:15 +0300

    feature/header-update
    add closing mobile menu when click on link

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,12 +2,33 @@
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
     const mobileBtnClose = document.querySelector("[data-menu-close]");
+    const mobileBtnClose1 = document.querySelector("[data-menu-close1]");
+    const mobileBtnClose2 = document.querySelector("[data-menu-close2]");
+    const mobileBtnClose3 = document.querySelector("[data-menu-close3]");
+    const mobileBtnClose4 = document.querySelector("[data-menu-close4]");
+    const mobileBtnClose5 = document.querySelector("[data-menu-close5]");
+  
   
     menuBtnRef.addEventListener("click", () => {
       mobileMenuRef.classList.toggle("is-open");
     })
 
     mobileBtnClose.addEventListener('click', () => {
+      mobileMenuRef.classList.toggle("is-open");
+    })
+    mobileBtnClose1.addEventListener('click', () => {
+      mobileMenuRef.classList.toggle("is-open");
+    });
+    mobileBtnClose2.addEventListener('click', () => {
+      mobileMenuRef.classList.toggle("is-open");
+    });
+    mobileBtnClose3.addEventListener('click', () => {
+      mobileMenuRef.classList.toggle("is-open");
+    });
+    mobileBtnClose4.addEventListener('click', () => {
+      mobileMenuRef.classList.toggle("is-open");
+    });
+    mobileBtnClose5.addEventListener('click', () => {
       mobileMenuRef.classList.toggle("is-open");
     });
   })()
